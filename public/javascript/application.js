3b4f4efad922d9dddfc8226a436df1c1679f7775
abkode <abozar.rahimi@gmail.com>
2016-08-25 06:06:28 +0000

    Flickr Photos API

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,4 +1,44 @@
-$(document).ready(function() {
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
-});
+$(function(){
+
+	var apiKey = '5416fef8ada2d0d6bef01f75135e956c';
+
+//    $("#btn").click(function(event){
+      	
+        $.ajax({
+        	
+        	url: 'https://api.flickr.com/services/rest/?method=flickr.photos.search&format=json',
+        	dataType: 'jsonp',
+        	jsonpCallback: 'jsonFlickrApi',
+        	data: {"tags":"lighthouses", api_key: apiKey},
+        	success: function(data){
+        		
+				$.each(data.photos.photo, function(i, item){
+          			$("#flickrImage").removeAttr("src");         		
+          			src = "http://farm"+ item.farm +".static.flickr.com/"+ item.server +"/"+ item.id +"_"+ item.secret +"_m.jpg";
+          			$("#slideshow").append("<div><img width='300px' height='200px' src= " + src + " /><h5>" + item.title + "</h5></div>")
+
+          		});
+
+				$("#slideshow > div:gt(0)").hide();
+				setInterval(function() { 
+				  $('#slideshow > div:first')
+				    .fadeOut(1000)
+				    .next()
+				    .fadeIn(1000)
+				    .end()
+				    .appendTo('#slideshow');
+				},  3000);
+
+
+          	},
+          	error: function(jqXHR, textStatus, errorThrown){
+				console.log("error!");
+				console.log(textStatus);
+				console.log(errorThrown);
+          	}     
+        });
+      //});
+   });
+
+ 
